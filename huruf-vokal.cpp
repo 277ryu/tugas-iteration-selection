080baeb447a6f1df51995e36270b4172265f8e14
277ryu <nasmiexc@gmail.com>
2025-09-28 17:33:01 +0700

    re-upload

---
+++ huruf-vokal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string Kalimat;
+    cout << "Pengecekan Jumlah Huruf Vokal" << endl;
+    cout << "-----------------------------" << endl;
+    cout << "Masukkan Kalimat: ";
+    getline(cin, Kalimat); // Menggunakan getline untuk membaca kalimat dengan spasi
+
+    int jmlVokal = 0;
+    int jmlHuruf = 0;
+
+    for (int i = 0; i < Kalimat.length(); ++i)
+    {
+        char ch = tolower(Kalimat[i]); // Mengubah karakter menjadi huruf kecil
+        if ((ch >= 'a' && ch <= 'z'))  // Memeriksa apakah karakter adalah huruf a-z
+        {
+            jmlHuruf++; // Menghitung jumlah huruf
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                jmlVokal++; // Menghitung jumlah huruf vokal
+            }
+        }
+    }
+
+    cout << "Jumlah Huruf: " << jmlHuruf << endl;
+    cout << "Jumlah Huruf Vokal: " << jmlVokal << endl;
+    return 0;
+}
+
+// Muhammad Affriza Ayman Nashmi
+// 25/559828/TK/63201
+// Teknologi Informasi - B
